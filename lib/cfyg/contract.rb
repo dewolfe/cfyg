89c24364c8a7d079937e3e3526511e54b3836713
Dominic DeWolfe <domdewolfe@gmail.com>
2018-03-04 21:27:39 -0500

    contract

--- lib/cfyg/contract.rb
+++ lib/cfyg/contract.rb
@@ -4,16 +4,24 @@ module Cfyg
 
     def initialize(**args)
       @sol_path = `which solc`
-      raise "unable to locate solc compiler" if @sol_path.empty?
+      raise "unable to locate solc compiler see http://solidity.readthedocs.io/en/develop/installing-solidity.html" if @sol_path.empty?
       @client = args.fetch(:client, Client.new)
     end
 
     def compile(contract: nil)
+      build_from_str(contract) if contract.is_a?(String)
+      build_from_file(contract) if contract.is_a?(File)
+
       client.send(method: 'eth_compileSolidity', params: [contract])
     end
 
 
     private
+   def build_from_str(contract)
+     temp_sol_file = File.open("temp.sol","w"){|file|file.write(contract)}
+     resonse = `solc --bin #{}`
+
+   end
 
 
   end
