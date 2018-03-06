70d43ac8567defc77b8e8ca3cb930ff6eca92f9a
Dominic DeWolfe <domdewolfe@gmail.com>
2018-03-06 06:28:24 -0500

    change contract and update spec

--- lib/cfyg/contract.rb
+++ lib/cfyg/contract.rb
@@ -1,27 +1,35 @@
 module Cfyg
+
   class Contract
-    attr_accessor :client
+    require 'tempfile'
+    attr_accessor :client, :sol_path, :contract
 
     def initialize(**args)
-      @sol_path = `which solc`
-      raise "unable to locate solc compiler see http://solidity.readthedocs.io/en/develop/installing-solidity.html" if @sol_path.empty?
+      @sol_path = `which solc`.chop
+      raise 'unable to locate solc compiler see http://solidity.readthedocs.io/en/develop/installing-solidity.html' if @sol_path.empty?
       @client = args.fetch(:client, Client.new)
+      @contract = args.fetch(:contract,"")
     end
 
-    def compile(contract: nil)
+    def compile
       build_from_str(contract) if contract.is_a?(String)
       build_from_file(contract) if contract.is_a?(File)
-
-      client.send(method: 'eth_compileSolidity', params: [contract])
     end
 
-
     private
-   def build_from_str(contract)
-     temp_sol_file = File.open("temp.sol","w"){|file|file.write(contract)}
-     resonse = `solc --bin #{}`
 
-   end
+    def build_from_str
+      binding.pry
+      resonse = `#{sol_path} --bin #{temp_file}`
+
+    end
+
+    def temp_file
+      temp_sol_file = Tempfile.new("temp.sol")
+      temp_sol_file.write(contract)
+      temp_sol_file.close
+      temp_sol_file.path
+    end
 
 
   end
--- spec/contract_spec.rb
+++ spec/contract_spec.rb
@@ -5,7 +5,7 @@ describe Cfyg::Contract do
     it "compiles solidity contracts" do
       sol_string = "contract test { function multiply(uint a) returns(uint d) {   return a * 7;   } }"
       client = Cfyg::Client.new
-      contract = Cfyg::Contract.new(client: client).compile(contract: sol_string)
+      contract = Cfyg::Contract.new(client: client,contract: sol_string).compile
       expect(contract).to be an_instance_of(Cfyg::Contract)
     end
   end
