ba63c94a02126eba4f4f8b1eddd7015e9824a4ff
Dominic DeWolfe <domdewolfe@gmail.com>
2018-03-06 19:24:50 -0500

    travis

--- spec/contract_spec.rb
+++ spec/contract_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 
 describe Cfyg::Contract do
 
-  before(:suit) do
+  before(:all) do
     @client = Cfyg::Client.new
   end
   context "solidity contract is a string" do
-    it "and it raises an error when passed an invalid contract" do
-      sol_string = "this is not a good contract"
+   # it "and it raises an error when passed an invalid contract" do
+   #   sol_string = "this is not a good contract"
 
-    end
+   # end
     it "and compiles solidity contracts" do
       sol_string = "contract test { function multiply(uint a) returns(uint d) {   return a * 7;   } }"
-      client = Cfyg::Client.new
-      contract = Cfyg::Contract.new(client: client,contract: sol_string).compile
+      contract = Cfyg::Contract.new(client: @client,contract: sol_string).compile
       expect(contract).to start_with "6060604052341561000"
     end
   end
